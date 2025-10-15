a0ed8c75990802b648d09d38b4fb5c6d302c3eb8
CrhistianTech <228229297+crhistian-bytebrisa@users.noreply.github.com>
2025-10-15 11:17:43 -0400

    Subir el proyecto

---
+++ Muestra de la api/Controllers/CharacterController.cs
@@ -0,0 +1,43 @@
+﻿using Muestra_de_la_api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Muestra_de_la_api.Controllers
+{
+    public  class CharacterController
+    {
+        HttpClient client;
+        public CharacterController()
+        {
+            client = new HttpClient();
+        }
+
+        public async Task<Characters> GetCharactersFirstPage()
+        {
+            var response = await client.GetAsync("https://rickandmortyapi.com/api/character/");
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                var result = JsonSerializer.Deserialize<Characters>(json);
+                return result;
+            }
+            return null;
+        }
+
+        public async Task<Characters> GetCharactersByUrl(string url)
+        {
+            var response = await client.GetAsync($"{url}");
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                var result = JsonSerializer.Deserialize<Characters>(json);
+                return result;
+            }
+            return null;
+        }
+    }
+}
---
+++ Muestra de la api/Program.cs
@@ -0,0 +1,82 @@
+﻿using Muestra_de_la_api.Controllers;
+using Muestra_de_la_api.Models;
+using System.Text.Json;
+
+namespace Muestra_de_la_api
+{
+    internal class Program
+    {
+        private static void WriteCharacters(List<Character> results)
+        {
+            results.ForEach(c => Console.WriteLine($"{c.id} - {c.name} - {c.gender} - {c.species} - {c.origin.name} - {c.status}"));
+        }
+        static async Task Main(string[] args)
+        {
+            var datos = new CharacterController();
+
+            var characters = await datos.GetCharactersFirstPage();
+
+            var results = characters.results;
+            var info = characters.info;
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Personajes de Rick and Morty:\n");
+                Console.WriteLine($"Hay un total de {info.count} personajes.\n");
+                Console.WriteLine($"Desde el: #{results.Select(x=> x.id).First()} hasta el #{results.Select(x=> x.id).Last()}\n\n");
+                WriteCharacters(results);
+
+                Console.WriteLine("\n1:Siguiente pagina.");
+                Console.WriteLine("2:Pagina anterior.");
+                var select = Console.ReadLine();
+
+                if (!int.TryParse(select,out int option))
+                {
+                    Console.Clear();
+                    Console.WriteLine("Opcion no valida. Solo se permiten los numeros 1 y 2.");
+                    Console.ReadKey();
+                    continue;
+                }
+
+                if(option != 1 && option != 2)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Elige entre uno o dos.");
+                    Console.ReadKey();
+                    continue;
+                }
+
+                if(option == 1 && info.next == null)
+                {
+                    Console.Clear();
+                    Console.WriteLine("No hay mas paginas.");
+                    Console.ReadKey();
+                    continue;
+                }
+
+                if(option == 2 && info.prev == null)
+                {
+                    Console.Clear();
+                    Console.WriteLine("No hay paginas anteriores.");
+                    Console.ReadKey();
+                    continue;
+                }
+
+                if(option == 1)
+                {
+                    characters = await datos.GetCharactersByUrl(info.next);
+                    results = characters.results;
+                    info = characters.info;
+                }
+               
+                if(option == 2)
+                {
+                    characters = await datos.GetCharactersByUrl(info.prev);
+                    results = characters.results;
+                    info = characters.info;
+                }
+            }
+        }
+    }
+}
